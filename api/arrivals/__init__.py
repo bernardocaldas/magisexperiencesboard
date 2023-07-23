0923bd5277c804523f05544ba5204cf3db2f0413
Bernardo Caldas <bernardo.caldas@mollie.com>
2023-07-23 14:31:17 +0100

    path

--- api/arrivals/__init__.py
+++ api/arrivals/__init__.py
@@ -1,14 +1,15 @@
 import logging
 import json
+import os
 import azure.functions as func
 
 
 # build an azure funtion that returns the contents of arrivals.json file as json
 def main(req: func.HttpRequest) -> func.HttpResponse:
     logging.info('Python HTTP trigger function processed a request.')
-
+    script_dir = os.path.dirname(__file__)
     # read the arrivals.json file
-    with open('arrivals.json') as json_file:
+    with open(os.path.join(script_dir, 'arrivals.json')) as json_file:
         data = json_file.read()
         arrivals = json.loads(data)
 
